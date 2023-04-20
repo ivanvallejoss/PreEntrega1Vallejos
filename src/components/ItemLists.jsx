86aadf204ed7bce143b6be5dfb33bb2d28ea9173
Ivan Vallejos <ivanvallejos06@gmail.com>
2023-04-20 03:33:17 -0600

    agregado de imagenes svg a los liss list item para jugar un poco

--- src/components/ItemLists.jsx
+++ src/components/ItemLists.jsx
@@ -1,10 +1,11 @@
+import totemFakeArt from '../assets/totemFake.svg'
 import '../estilos/ItemList.css'
-
 
 export const ItemList = ({greetings}) =>{
     
     return (
         <div className="itemContainer">
+            <img src={totemFakeArt} alt="" className='fakeIcon'/>
             <h3>{greetings}</h3>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil optio placeat explicabo, minima aspernatur, soluta delectus unde reprehenderit omnis deleniti facere, cupiditate voluptates quidem temporibus! Sunt odit iste assumenda laboriosam!</p>
         </div>
