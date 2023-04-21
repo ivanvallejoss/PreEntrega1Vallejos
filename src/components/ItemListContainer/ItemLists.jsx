5f8a7f9b0b52be796e050e7ae5a209f19adb9a61
Ivan Vallejos <ivanvallejos06@gmail.com>
2023-04-21 16:45:13 -0600

    estilos

--- src/components/ItemListContainer/ItemLists.jsx
+++ src/components/ItemListContainer/ItemLists.jsx
@@ -5,8 +5,10 @@ export const ItemList = ({greetings}) =>{
     return (
         <div className="itemContainer">
             <img src={'./public/totemFake.svg'} alt="" className='fakeIcon'/>
+            <div className='itemContainer__info'>
             <h3>{greetings}</h3>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil optio placeat explicabo, minima aspernatur, soluta delectus unde reprehenderit omnis deleniti facere, cupiditate voluptates quidem temporibus! Sunt odit iste assumenda laboriosam!</p>
+            </div>
         </div>
     )
 }
